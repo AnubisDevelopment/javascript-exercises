2e555257bae441c39aeff86814d213d234e046d9
AnubisDevelopment <zasiobaldelomar@gmail.com>
2025-01-19 13:48:14 -0800

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(x) {
+    let splitString = x.split('')
+    let reverseString = splitString.reverse()
+    let joinString = reverseString.join('')
+    return joinString
 };
 
 // Do not edit below this line
