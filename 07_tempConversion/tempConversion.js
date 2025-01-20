dc56fd5af1475eb6d91ae0b2ef5bf269b5ecef94
AnubisDevelopment <zasiobaldelomar@gmail.com>
2025-01-19 16:00:37 -0800

    Complete exercises 1 - 7
    Finished exercises 1 - 7 focusing on creating functions to pass test specifications.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let conversion = (f-32) * 5/9
+  if(conversion !== Number.isInteger(conversion)){
+    let decimalValue = conversion.toFixed(1)
+    return Number(decimalValue)
+  } else {
+    return conversion
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let conversion = (c * 9/5) + 32
+    if(conversion !== Number.isInteger(conversion)){
+      let decimalValue = conversion.toFixed(1)
+      return Number(decimalValue)
+    } else {
+      return conversion
+    }
+  //return conversion.toFixed(1)
 };
 
+//(x * 9/5) +32 c to f
+// (x-32)* 5/9 f to c
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
