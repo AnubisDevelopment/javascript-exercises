9de9ebc2256714ff152c17e8f4eda88f283159d5
AnubisDevelopment <zasiobaldelomar@gmail.com>
2025-02-13 14:49:11 -0800

    Complete exercise 8
    Completed exercise 8 approved by all test cases

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	return x.reduce((a,b)=> 
+    a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  return x.reduce((a,b)=> a * b)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x,y)
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(x) {
+  if (x == 1 || x == 0){
+    return 1
+  } else {
+  let sum = 1
+	 for(let i = 1; i <= x; i++){
+    sum *= i 
+  }
+  return sum
+}};
 
 // Do not edit below this line
 module.exports = {
